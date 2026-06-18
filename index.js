5787cd89fd82d9098a052d3759360f308b6be2e7
ayupandey12 <ayushpandey9802@gmail.com>
2026-06-18 08:32:21 +0530

    updated task cli app

--- index.js
+++ index.js
@@ -2,19 +2,32 @@ import path from "node:path";
 import * as fs from 'node:fs';
 
 // 1. Correctly build the path relative to the current script file
-const p = path.join(import.meta.dirname, 'ap.json');
-
 // 2. Correctly order the callback parameters: (error, data)
 // 3. Add 'utf8' encoding to get readable text instead of a binary Buffer
-fs.readFile(p, 'utf8', (error, data) => {
+
+const p1 = path.join(import.meta.dirname, 'app.json');
+
+const data=fs.readFileSync(p1, 'utf8', (error, data) => {
     if (error) {
         // Correct way to throw a standard Error object
         throw new Error("Failed to read file"); 
     }
-    console.log(JSON.parse(data).ayush);
+    return Array(JSON.parse(data));
 });
-const p1 = path.join(import.meta.dirname, 'app.json');
-const oknowauio = { status: "success", timestamp: Date.now() };
-fs.appendFile(p1, JSON.stringify(oknowauio) + '\n',(error)=>{
+console.log(data);
+let array=[];
+    if(data!=null||data!="") array.push(data);
+    console.log(array.length)
+    const size=array.length;
+   let id= size>0?array[size-1].id:0;
+   const task={
+    id:id+1,
+    description:"ok now this is new task",
+    progress:"todo",
+    createdTime:Date.now(),
+    updatedTime:Date.now()
+}
+   array.push(task)
+fs.appendFile(p1, JSON.stringify(array) + '\n',(error)=>{
     if(error) throw new error("not appended")
 });
