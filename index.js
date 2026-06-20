d5de8b0daac9c49f99e2d71c18bca970be3fe96a
ayupandey12 <ayushpandey9802@gmail.com>
2026-06-20 18:29:11 +0530

    working cli with local storage file

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ import * as fs from 'node:fs';
 
 
 const d=process.argv
-const p =path.join(import.meta.dirname,'./app.json');
-
-console.log(d,p)
+const p =path.join(import.meta.dirname,'./ap.json');
+const d1=fs.readFileSync(p,'utf8');
+const t=JSON.parse(d1);
+t[1].ayush=13;
+t.push({ayush:20,name:"ok3"})
+fs.writeFileSync(p,JSON.stringify(t),'utf8');
+console.log(d,p,t)
